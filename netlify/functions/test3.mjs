3eb93786e5903258f0ae49f2c6d651168769d367
591mert <150172098+591mert@users.noreply.github.com>
2026-07-17 16:13:24 +0300

    test: clsx import test

---
+++ netlify/functions/test3.mjs
@@ -0,0 +1,8 @@
+import clsx from "clsx";
+
+export default async () => {
+  return new Response(JSON.stringify({ ok: true, clsx: clsx("a", "b") }), {
+    status: 200,
+    headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
+  });
+};
