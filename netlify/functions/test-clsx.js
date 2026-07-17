9f6c0a98da9e74d063b24a18987f513f476a571d
591mert <150172098+591mert@users.noreply.github.com>
2026-07-17 16:14:15 +0300

    test: clsx import test (.js)

--- netlify/functions/test-clsx.js
+++ netlify/functions/test-clsx.js
@@ -1,7 +1,7 @@
 import clsx from "clsx";
 
 export default async () => {
-  return new Response(JSON.stringify({ ok: true, clsx: clsx("a", "b") }), {
+  return new Response(JSON.stringify({ ok: true, result: clsx("a", "b") }), {
     status: 200,
     headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
   });
